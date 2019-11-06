3f0fed24ac1866e5f3d5e2d866ef630e2b71849f
ALEX NOGUEIRA DA SILVA <tec.alexnogueira@gmail.com>
2019-11-06 08:14:44 -0300

    alteracao
    add

--- App/Modos de exibição/Layouts/Header.php
+++ App/Modos de exibição/Layouts/Header.php
@@ -2,5 +2,5 @@
 	<div class="sidebar-toggle-box">
 		<div data-original-title="Toggle Navigation" data-placement="right" class="fa fa-bars tooltips"></div>
 	</div>
-	<a href="http://<?php echo APP_HOST; ?>" class="logo">DEVMEDIA - PHP </a>
+	<a href="http://<?php echo APP_HOST; ?>" class="logo">DevAction - Alagoas </a>
 </header>
